30e44c9cfc298395a5cce0f63bc5c0afc2776335
QuentinC26 <q.zuzlewski@gmail.com>
2026-04-09 16:15:39 +0200

    i add a base for the home page

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <Router>
+    <div>
+      <h1>Page d'Accueil</h1>
+      <p>Bienvenue sur la page d'accueil !</p>
+    </div>
+    </Router>
+  );
+}
+
+export default Home;
