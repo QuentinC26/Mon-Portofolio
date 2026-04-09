a1760b0b0a7c5d69cd33a282be67a1a981be9864
QuentinC26 <q.zuzlewski@gmail.com>
2026-04-09 16:16:13 +0200

    i add a base page for my project page

---
+++ front-end/src/pages/About_me.jsx
@@ -0,0 +1,12 @@
+function About_me() {
+  return (
+    <Router>
+    <div>
+      <h1>À propos</h1>
+      <p>/</p>
+    </div>
+    </Router>
+  );
+}
+
+export default About_me;
---
+++ front-end/src/pages/My_projects.jsx
@@ -0,0 +1,12 @@
+function My_Projects() {
+  return (
+    <Router>
+    <div>
+      <h1>Mes projets</h1>
+      <p>/</p>
+    </div>
+    </Router>
+  );
+}
+
+export default My_Projects;
