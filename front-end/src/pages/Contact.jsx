94d8548b46080abb698051b582227d5e42dc8b97
QuentinC26 <q.zuzlewski@gmail.com>
2026-04-12 17:31:30 +0200

    i add three import, the different tags and the content of the page

--- front-end/src/pages/Contact.jsx
+++ front-end/src/pages/Contact.jsx
@@ -1,11 +1,18 @@
+import contacter_moi from "../assets/contacter_moi.png"
+import email from "../assets/email.png"
+import linkedin from "../assets/linkedin.png"
+
 function Contact() {
   return (
-    <Router>
     <div>
-      <h1>Contact</h1>
-      <p>/</p>
+      <br></br>
+      <img src={contacter_moi} alt="Me contacter"/>
+      <br></br>
+      <br></br>
+      <p><img src={email} alt="Mon email"/> : qzuzlewski@gmail.com</p>
+      <p>Linkedin : <a href="https://www.linkedin.com/in/quentinzuzlewski/"> <img src={linkedin} alt="Mon Linkedin"/></a></p>
+      <br></br>
     </div>
-    </Router>
   );
 }
 
