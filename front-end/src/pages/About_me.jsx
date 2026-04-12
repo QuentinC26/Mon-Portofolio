e27e83367d6584b0b13028d05a1eec03acb57f9b
QuentinC26 <q.zuzlewski@gmail.com>
2026-04-12 17:30:42 +0200

    i add a different tag and the content of the page

--- front-end/src/pages/About_me.jsx
+++ front-end/src/pages/About_me.jsx
@@ -1,11 +1,24 @@
 function About_me() {
   return (
-    <Router>
     <div>
-      <h1>À propos</h1>
-      <p>/</p>
+      <br></br>
+      <h2>Qui suis-je ?</h2>
+      <p>Je suis développeur web full stack, avec une préférence pour le front-end et une forte sensibilité créative. J’aime travailler sur des projets variés, qu’ils soient similaires ou complètement à l’opposé les uns des autres. </p>
+      <h2>Mon parcours :</h2>
+      <p>À l’issue de ma formation de développeur web à Holberton School, j’ai réalisé plusieurs projets concrets afin de renforcer mes compétences techniques et personnelles. Aujourd’hui, je continue à développer mes compétences à travers de nouveaux projets et reste ouvert à toute opportunité.</p>
+      <br></br>
+      <h2>Comment je travaille ? :</h2>
+      <p>Avant de commencer un projet, je prends le temps de réfléchir aux objectifs et de me renseigner sur les outils et technologies les plus adaptés. J’accorde une grande importance à la lisibilité du code, avec des noms explicites pour les variables et les fichiers, ainsi que des commentaires lorsque c’est nécessaire. J’organise mes projets de manière structurée, notamment en séparant le front-end, le back-end et la base de données à l’aide de Docker. Je travaille de façon progressive, en commençant généralement par le back-end avant de développer le front-end. Enfin, j’utilise une approche agile avec un planning me permettant de suivre l’avancement du projet et de m’adapter en cas d’imprévus. </p>
+      <br></br>
+      <h2>Ce que je sais faire :</h2>
+      <p>Lors de ma formation à Holberton School, j’ai travaillé avec plusieurs langages et technologies comme Python, JavaScript, HTML, CSS et MySQL.
+      Mes projets m’ont ensuite permis de découvrir Django, React, Flask et Docker, avec un intérêt particulier pour Django et son organisation claire côté back-end.</p>
+      <br></br>
+      <h2>Ce que je recherche :</h2>
+      <p>Je suis actuellement à la recherche d’un stage afin de continuer à développer mes compétences techniques et mes soft skills. Je reste ouvert à des opportunités en back-end, en front-end ou en full stack.</p>
+      <br></br>
+      <br></br>
     </div>
-    </Router>
   );
 }
 
