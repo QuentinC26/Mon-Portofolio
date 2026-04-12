2e41fd5ea4a8fb1907e226fa693d7b51bbf54495
QuentinC26 <q.zuzlewski@gmail.com>
2026-04-12 16:56:49 +0200

    i add one import, different tags and the content of the page

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,11 +1,21 @@
+import quentinzuzlewski from "../assets/quentinzuzlewski.png"
+
 function Home() {
   return (
-    <Router>
     <div>
-      <h1>Page d'Accueil</h1>
-      <p>Bienvenue sur la page d'accueil !</p>
+      <br></br>
+      <h2>Quentin Zuzlewski</h2>
+      <br></br>
+      <img src={quentinzuzlewski} alt="Quentin Zuzlewski"/>
+      <br></br>
+      <br></br>
+      <h2>Développeur web</h2>
+      <br></br>
+      <a href="https://www.canva.com/design/DAGr0MT1XQw/Deed52pzNYvUyZpdWZDE2Q/view?utm_content=DAGr0MT1XQw&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h52064ce82c">Consulter mon CV</a>
+      <br></br>
+      <br></br> 
+      <br></br>
     </div>
-    </Router>
   );
 }
 
