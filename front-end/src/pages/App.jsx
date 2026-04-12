f0ec69361c1bf2332498888d5ee1a3a5fc393021
QuentinC26 <q.zuzlewski@gmail.com>
2026-04-12 16:56:06 +0200

    i add multiples import and the footer

--- front-end/src/pages/App.jsx
+++ front-end/src/pages/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About_me from './pages/About_me';
-import My_Projects from './pages/My_Projects';
-import Contact from './pages/contact';
+import Home from './Home';
+import About_me from './About_me';
+import My_projects from './My_projects';
+import Contact from './Contact';
 import './App.css'
+import github from "../assets/github.png"
+import linkedin from "../assets/linkedin.png"
 
 function App() {
   return (
+    <>
     <Router>
       <nav>
         <ul>
@@ -20,10 +23,16 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/à_propos" element={<About_me />} />
-        <Route path="/mes_projets" element={<My_Projects />} />
+        <Route path="/mes_projets" element={<My_projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
+      <footer>
+      <a href="https://github.com/QuentinC26"> <img src={github} alt="Mon Github"/></a>
+      <a href="https://www.linkedin.com/in/quentinzuzlewski/"> <img src={linkedin} alt="Mon Linkedin"/></a>
+      <p class="copyright">©2026 Quentin Zuzlewski. Tous droits réservés.</p>
+    </footer>
+    </>
   );
 }
 
