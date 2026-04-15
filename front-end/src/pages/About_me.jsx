a8c40cdf719494fd5d2d327071e8028573deff5f
QuentinC26 <q.zuzlewski@gmail.com>
2026-04-15 12:58:15 +0200

    i replace the card by tabs

--- front-end/src/pages/About_me.jsx
+++ front-end/src/pages/About_me.jsx
@@ -1,28 +1,88 @@
+import { useState } from "react";
+import quentinzuzlewski from "../assets/quentinzuzlewski.png"
+
 function About_me() {
+  const [tab, setTab] = useState("who");
+  
   return (
     <div>
-      <br></br>
+      <br />
       <h2>À propos de moi :</h2>
-      <br></br>
-      <div className="cards-grid">
-      <div className="card"><h2>Qui suis-je ?</h2>
-      <p>Je suis développeur web full stack, avec une préférence pour le front-end et une forte sensibilité créative. J’aime travailler sur des projets variés, qu’ils soient similaires ou complètement à l’opposé les uns des autres. </p></div>
-      <br></br>
-      <div className="card"><h2>Mon parcours :</h2>
-      <p>À l’issue de ma formation de développeur web à Holberton School, j’ai réalisé plusieurs projets concrets afin de renforcer mes compétences techniques et personnelles. Aujourd’hui, je continue à développer mes compétences à travers de nouveaux projets et reste ouvert à toute opportunité.</p></div>
-      <br></br>
-       <div className="card"><h2>Comment je travaille ? :</h2>
-      <p>Avant de commencer un projet, je prends le temps de réfléchir aux objectifs et de me renseigner sur les outils et technologies les plus adaptés.<br></br><br></br> J’accorde une grande importance à la lisibilité du code, avec des noms explicites pour les variables et les fichiers, ainsi que des commentaires lorsque c’est nécessaire.<br></br><br></br> J’organise mes projets de manière structurée, notamment en séparant le front-end, le back-end et la base de données à l’aide de Docker.<br></br><br></br> Je travaille de façon progressive, en commençant généralement par le back-end avant de développer le front-end.<br></br><br></br> Enfin, j’utilise une approche agile avec un planning me permettant de suivre l’avancement du projet et de m’adapter en cas d’imprévus. </p></div>
-      <br></br>
-       <div className="card"><h2>Ce que je sais faire :</h2>
-      <p>Lors de ma formation à Holberton School, j’ai travaillé avec plusieurs langages et technologies comme Python, JavaScript, HTML, CSS et MySQL.
-      Mes projets m’ont ensuite permis de découvrir Django, React, Flask et Docker, avec un intérêt particulier pour Django et son organisation claire côté back-end.</p></div>
-      <br></br>
-      <div className="card"><h2>Ce que je recherche :</h2>
-      <p>Je suis actuellement à la recherche d’un stage afin de continuer à développer mes compétences techniques et mes soft skills. Je reste ouvert à des opportunités en back-end, en front-end ou en full stack.</p></div>
+      <br />
+
+      <div className="tabs">
+        <button onClick={() => setTab("who")}>Qui suis-je ?</button>
+        <button onClick={() => setTab("path")}>Mon parcours</button>
+        <button onClick={() => setTab("work")}>Comment je travaille</button>
+        <button onClick={() => setTab("skills")}>Ce que je sais faire</button>
+        <button onClick={() => setTab("search")}>Ce que je recherche</button>
       </div>
-      <br></br>
-      <br></br>
+
+      <br />
+
+      {tab === "who" && (
+        <div>
+          <h2>Qui suis-je ?</h2>
+          <br />
+          <div className="tabs_onetabs">
+          <img src={quentinzuzlewski} alt="Quentin Zuzlewski"/>
+          <p>
+            Je suis développeur web full stack, avec une préférence pour le front-end et une forte sensibilité créative.
+            J’aime travailler sur des projets variés, qu’ils soient similaires ou complètement à l’opposé les uns des autres.
+          </p>
+        </div>
+        </div>
+      )}
+
+      {tab === "path" && (
+        <div className="tabs_onetabs">
+          <h2>Mon parcours :</h2>
+          <p>
+            À l’issue de ma formation de développeur web à Holberton School, j’ai réalisé plusieurs projets concrets
+            afin de renforcer mes compétences techniques et personnelles. Aujourd’hui, je continue à développer mes
+            compétences à travers de nouveaux projets et reste ouvert à toute opportunité.
+          </p>
+        </div>
+      )}
+
+      {tab === "work" && (
+        <div className="tabs_onetabs">
+          <h2>Comment je travaille ? :</h2>
+          <p>
+            Avant de commencer un projet, je prends le temps de réfléchir aux objectifs et de me renseigner sur les outils
+            et technologies les plus adaptés.
+            J’accorde une grande importance à la lisibilité du code, avec des noms explicites pour les variables et les fichiers,
+            ainsi que des commentaires lorsque c’est nécessaire.
+            J’organise mes projets de manière structurée, notamment en séparant le front-end, le back-end et la base de données
+            à l’aide de Docker.
+            Je travaille de façon progressive, en commençant généralement par le back-end avant de développer le front-end.
+            Enfin, j’utilise une approche agile avec un planning me permettant de suivre l’avancement du projet et de m’adapter
+            en cas d’imprévus.
+          </p>
+        </div>
+      )}
+
+      {tab === "skills" && (
+        <div className="tabs_onetabs">
+          <h2>Ce que je sais faire :</h2>
+          <p>
+            Lors de ma formation à Holberton School, j’ai travaillé avec plusieurs langages et technologies comme Python,
+            JavaScript, HTML, CSS et MySQL. Mes projets m’ont ensuite permis de découvrir Django, React, Flask et Docker,
+            avec un intérêt particulier pour Django et son organisation claire côté back-end.
+          </p>
+        </div>
+      )}
+
+      {tab === "search" && (
+        <div className="tabs_onetabs">
+          <h2>Ce que je recherche :</h2>
+          <p>
+            Je suis actuellement à la recherche d’un stage afin de continuer à développer mes compétences techniques et mes soft skills.
+            Je reste ouvert à des opportunités en back-end, en front-end ou en full stack.
+          </p>
+        </div>
+      )}
+      <br />
     </div>
   );
 }
