41658fa6ce02c24fdb57466b88aab0c721404173
QuentinC26 <q.zuzlewski@gmail.com>
2026-04-16 23:23:30 +0200

    i add the card, the good size of picture and the useState for have see more or see less

--- front-end/src/pages/My_projects.jsx
+++ front-end/src/pages/My_projects.jsx
@@ -1,31 +1,57 @@
+import { useState } from "react";
 import logo_AE from "../assets/logo_AE.png"
 import hbnb_logo from "../assets/hbnb_logo.png"
 
 function My_projects() {
+  const [openCard, setOpenCard] = useState(null);
+
   return (
     <div>
     <br></br>
-    <h2>Mes projets</h2>
+    <h2>Mes projets :</h2>
     <br></br>
-    <u><b> Voici les projets que j’ai réalisés ou sur lesquels je travaille actuellement</b></u>
-    <br></br>
-    <h3>Autistic Eye</h3>
-    <p>(Août 2025 - en cours...)</p>
-    <br></br>
-    <img src={logo_AE} alt="Logo Autistic Eye"/>
+    <div className="cards-grid">
+    <div className="card ">
+    <div className="card-img"><img src={logo_AE} alt="Logo Autistic Eye"/></div>
+    <button><a href="https://github.com/QuentinC26/Autistic-Eye">Autistic Eye</a></button>
     <br></br>
     <br></br>
-    <p>Autistic Eye est un réseau social communautaire dédié aux personnes autistes et à leurs proches. Il a été créé pour lutter contre l’isolement en offrant un espace d’échange, d’entraide et de partage d’expériences. La plateforme permet d’accéder facilement à des informations utiles sur de nombreux sujets liés à l’autisme, afin d’améliorer le quotidien des utilisateurs.</p>
+    <div className="card-date">(Août 2025 - en cours...)</div>
+    <div className="card-content"> {openCard === "AE" ? (
+    <>
+    Réseau social dédié aux personnes autistes et à leurs proches, visant à lutter contre l’isolement en favorisant l’échange et le partage d’expériences. Accès à des ressources et informations pour améliorer le quotidien des utilisateurs.
+    </>
+    ) : (
+    <>
+    Réseau social dédié aux personnes autistes et à leurs proches...
+    </>
+    )}
     <br></br>
-    <h3>Hbnb</h3>
-    <p>(Mai 2025 - Août 2025)</p>
     <br></br>
-    <img src={hbnb_logo} alt="Logo Hbnb"/>
+    <button onClick={() => setOpenCard(openCard === "AE" ? null : "AE")}>{openCard === "AE" ? "Voir moins" : "Voir plus"}</button>
+    </div>
+    </div>
+    <div className="card ">
+    <div className="card-img"><img src={hbnb_logo} alt="Logo Hbnb"/></div>
+    <button><a href="https://github.com/QuentinC26/holbertonschool-hbnb">Hbnb</a></button>
     <br></br>
     <br></br>
-    <p>Hbnb est un projet académique de la Holberton School, réalisé en groupe avec <a href="https://github.com/EmmanuelMOUMBOUILOU">EmmanuelMOUMBOUILOU</a> et <a href="https://github.com/Dj-kvt">Dj-kvt</a> pour la partie back-end, puis en individuel pour la partie front-end. Le but de ce projet était de créer une version simplifiée du site Airbnb.</p>
+    <div className="card-date">(Mai 2025 - Août 2025)</div>
+    <div className="card-content"> {openCard === "HBNB" ?  (
+    <>
+    Projet académique inspiré d’Airbnb, réalisé en groupe pour le back-end puis en autonomie pour le front-end. Développement d’une application web complète avec gestion des utilisateurs et des logements.
+    </>
+    ) : (
+    <>
+    Projet académique inspiré d’Airbnb, réalisé en groupe pour le back-end puis en autonomie pour le front-end.
+    </>
+    )}
     <br></br>
     <br></br>
+    <button onClick={() => setOpenCard(openCard === "HBNB" ? null : "HBNB")}>{openCard === "HBNB" ? "Voir moins" : "Voir plus"}</button>
+    </div>
+    </div>
+    </div>
     </div>
   );
 }
