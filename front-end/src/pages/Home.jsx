c53c95aa131c60b874c69fa614db64fd60ac41fc
QuentinC26 <q.zuzlewski@gmail.com>
2026-04-18 13:30:24 +0200

    i remove all br tags

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -3,18 +3,10 @@ import quentinzuzlewski from "../assets/quentinzuzlewski.png"
 function Home() {
   return (
     <div>
-      <br></br>
       <h2>Quentin Zuzlewski</h2>
-      <br></br>
       <img className="home_img" src={quentinzuzlewski} alt="Quentin Zuzlewski"/>
-      <br></br>
-      <br></br>
       <h2>Développeur web</h2>
-      <br></br>
       <a className="a_cv" href="https://www.canva.com/design/DAGr0MT1XQw/Deed52pzNYvUyZpdWZDE2Q/view?utm_content=DAGr0MT1XQw&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h52064ce82c">Consulter mon CV</a>
-      <br></br>
-      <br></br> 
-      <br></br>
     </div>
   );
 }
